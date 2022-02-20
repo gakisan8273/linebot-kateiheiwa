7133968df46fc4ecacb532edeb1d58172acb12fe
gakisan8273 <mint.daa.a2@gmail.com>
2022-02-21 01:20:33 +0900

    応答メッセージテスト

--- handler.py
+++ handler.py
@@ -1,14 +1,29 @@
 import json
 import os
+from logging import getLogger
+logger = getLogger(__name__)
 
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
 
 line_bot_api = LineBotApi(os.environ['LINE_ACCESS_TOKEN'])
-webhook = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
+webhook_handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
 def handler(event, context):
+    signature = event['headers']['x-line-signature']
+    try:
+        # handleの引数はJSON文字列（パース前）
+        webhook_handler.handle(event['body'], signature)
+        print('authenticated')
+        # event['body']はJSON文字列なのでパースする
+        body = json.loads(event['body'])
+        reply_token = body['events'][0]['replyToken']
+        line_bot_api.reply_message(reply_token, TextSendMessage('応答メッセージ'))
+    except InvalidSignatureError:
+        print('not authenticated')
+        print(InvalidSignatureError)
+
     line_bot_api.push_message(os.environ['MY_LINE_USER_ID'], TextSendMessage(text='test'))
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
