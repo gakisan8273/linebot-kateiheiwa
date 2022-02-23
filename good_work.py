733479d1253880fa5f09d9b5cc5489ef6d5f8622
gakisan8273 <mint.daa.a2@gmail.com>
2022-02-24 00:51:04 +0900

    関数の整理

--- good_work.py
+++ good_work.py
@@ -11,20 +11,6 @@ line_bot_api = LineBotApi(os.environ['LINE_ACCESS_TOKEN'])
 webhook_handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
 def handler(event, context):
-    # signature = event['headers']['x-line-signature']
-    # try:
-    #     # handleの引数はJSON文字列（パース前）
-    #     webhook_handler.handle(event['body'], signature)
-    #     print('authenticated')
-    #     # event['body']はJSON文字列なのでパースする
-    #     body = json.loads(event['body'])
-    #     reply_token = body['events'][0]['replyToken']
-    #     line_bot_api.reply_message(reply_token, TextSendMessage('応答メッセージ'))
-    # except InvalidSignatureError:
-    #     print('not authenticated')
-    #     print(InvalidSignatureError)
-
-    # line_bot_api.push_message(os.environ['MY_LINE_USER_ID'], TextSendMessage(text='test'))
     message_actions = [
         MessageAction(label='元気！', text='> 元気！'),
         MessageAction(label='ちょっとしんどい', text='> ちょっとしんどい'),
@@ -46,12 +32,3 @@ def handler(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
