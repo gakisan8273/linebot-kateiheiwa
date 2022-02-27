0451743ab76fd1af7b119083ef2e5dc11a53661a
gakisan8273 <mint.daa.a2@gmail.com>
2022-02-28 01:46:39 +0900

    DRYに

--- good_work.py
+++ good_work.py
@@ -1,17 +1,15 @@
 import json
 import os
-from logging import getLogger
-logger = getLogger(__name__)
 
 from linebot import (LineBotApi, WebhookHandler)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageAction, PostbackAction)
+from linebot.models import (TemplateSendMessage, ButtonsTemplate, PostbackAction)
 from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
 
 line_bot_api = LineBotApi(os.environ['LINE_ACCESS_TOKEN'])
 webhook_handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
 def handler(event, context):
-    status_map = {
+    status_dict = {
         'genki': {
             'score': 5,
             'label': '元気！',
@@ -29,18 +27,18 @@ def handler(event, context):
             'label': 'もう無理',
         },
     }
-    postback_actions = [
-        PostbackAction(data=status_map['genki']['score'], label=status_map['genki']['label'], text='> ' + status_map['genki']['label']),
-        PostbackAction(data=status_map['a_little_sindoi']['score'], label=status_map['a_little_sindoi']['label'], text='> ' + status_map['a_little_sindoi']['label']),
-        PostbackAction(data=status_map['sindoi']['score'], label=status_map['sindoi']['label'], text='> ' + status_map['sindoi']['label']),
-        PostbackAction(data=status_map['muri']['score'], label=status_map['muri']['label'], text='> ' + status_map['muri']['label']),
-    ]
+    postback_actions = []
+    for status, item in status_dict.items():
+        postback_actions.append(PostbackAction(data=item['score'], label=item['label'], text='> ' + item['label']))
+
     title = '今日もお仕事お疲れ様！'
     text = '体調はどうかな？'
     buttuns_template = ButtonsTemplate(text=text, title=title, actions=postback_actions)
+    # TODO: 例外処理
     line_bot_api.push_message(os.environ['MY_LINE_USER_ID'], TemplateSendMessage(alt_text=title, template=buttuns_template))
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "お疲れ様メッセージを送信しました。",
         "input": event
     }
 
